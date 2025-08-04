7b14178b4f93ce697279ff5cfb98cb7d39ba7f87
Dhiraj Kafle <dhirajkafle19@gmail.com>
2025-08-04 10:29:17 +0545

    Friend class concept in OOP of c++

--- C++/2_OOP_Object_Oriented_Programming/From_beginner_to_pro/2_friend_class/friend_class.cpp
+++ C++/2_OOP_Object_Oriented_Programming/From_beginner_to_pro/2_friend_class/friend_class.cpp
@@ -1,32 +1,60 @@
 #include<iostream>
 using namespace std;
+
+
+
 class test
 {
     int num1, num2;
+    
     public:
-    friend class first;
+    	friend class first;				// since, first class is declared as friend of test class
+    									// (which means, first class can access private members of test class)
 };
+
+
+
 class first
 {
     int res;
+    
     public:
-    void sum();
+    	void sum();
 };
+
+
+
 
 void first:: sum()
 {
-    test t;
-    cout<<"\n Enter 1st number";
-    cin>>t.num1;
-    cout<<"Enter 2nd number";
-    cin>>t.num2;
-    res=t.num1+t.num2;
-    cout<<"\n Sum is"<<res;
+	
+    test t;								// creating test object/instance (i.e t) inside sum function of first class
+    									// (yesari arko class ko object banauna tw paincha, yesko lagi friendship banai rakhna pardaina)
+    
+    
+    cout << "\n Enter 1st number: ";
+    cin >> t.num1;					// yesari, arko class ko private member/maal (i.e num1) lai aafno class ko function ma use garna chai friendship hunai parcha, ani ball milcha.
+    
+    
+    cout << "\n Enter 2nd number: ";
+    cin >> t.num2;					// yesari, arko class ko private member/maal (i.e num2) lai aafno class ko function ma use garna chai friendship hunai parcha, ani ball milcha.
+    
+    
+    res = t.num1 + t.num2;
+    
+    
+    cout<<"\n Sum is: " << res << endl;
 }
+
+
+
 int main()
 {
+	// creating object of first class (i.e f)
     first f;
+    
     f.sum();
+    
     return 0;
 
 }
